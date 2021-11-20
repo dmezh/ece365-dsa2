90bc3d5bbb0c76796f0e40ad6ef9eeab8d2cb0fb
dmezh <54985569+dmezh@users.noreply.github.com>
2021-11-20 17:21:36 -0500

    Add project 2

---
+++ a2/hash.cpp
@@ -0,0 +1,131 @@
+#include "hash.h"
+
+#include <iostream>
+#include <string>
+#include <tuple>
+
+#define HASH_SEED 5381
+#define MAX_LOAD_FACTOR 0.75
+
+int hashTable::insert(std::string_view key, void *pv) {
+    if (static_cast<float>(this->filled) / this->capacity > MAX_LOAD_FACTOR) {
+        if (!this->rehash()) return 2;
+    }
+    
+    bool exists; Hash hash;
+    std::tie(exists, hash) = this->findPos(key);
+    if (exists)
+        return 1;
+
+    hashItem *i = &this->data[hash];
+    i->key = key;
+    i->isOccupied = true;
+    i->isDeleted = false;
+    i->pv = pv;
+
+    this->filled++;
+
+    return 0;
+}
+
+bool hashTable::rehash() {
+    Hash newSize = getPrime(this->capacity * 2);
+
+    try {
+        std::vector<hashItem> newData(newSize);
+        std::vector<hashItem> oldData = this->data;
+        this->data.swap(newData);
+        this->capacity = newSize;
+
+        for (auto item : oldData) {
+            if (item.isOccupied && !item.isDeleted) {
+                this->insert(item.key);
+            }
+        }
+    } catch (std::bad_alloc) {
+        return false;
+    }
+
+    return true;
+}
+
+std::pair<bool, Hash> hashTable::findPos(std::string_view key) {
+    Hash hash = this->hash(key);
+
+    while (this->data[hash].isOccupied && !this->data[hash].isDeleted) { // linear probing
+        if (this->data[hash].key == key)
+            return {true, hash};
+
+        if (++hash >= this->capacity) // wrap around
+            hash = 0;
+    }
+
+    return {false, hash};
+}
+
+hashTable::hashTable(int size) {
+    this->capacity = getPrime(size);
+    this->data.resize(this->capacity);
+}
+
+unsigned hashTable::getPrime(int size) {
+    static const int primes[] = {
+        2,11,19,31,53,71,109,163,233,547,877,1229,1993,4001,7001,
+        10181,17393,26407,38923,52363,66107,84673,99551,121441,200033,
+        300497,526441,1050139,2711899,5630279,8994599,12353267,15485863
+    };
+
+    for (auto prime : primes)
+        if (prime > size)
+            return prime;
+
+    std::cerr << "Error: Size of hash table exceeds maximum listed prime!" << "\n";
+    abort(); // maybe generate more primes on the spot instead of giving up
+}
+
+// http://www.cse.yorku.ca/~oz/hash.html (djb2)
+Hash hashTable::hash(std::string_view key) {
+    Hash hash = HASH_SEED;
+    for (char c : key)
+        hash = ((hash << 5) + hash) + c;
+    return hash % this->capacity;
+}
+
+bool hashTable::contains(std::string_view key) {
+    bool exists;
+    std::tie(exists, std::ignore) = this->findPos(key);
+    return exists;
+}
+
+bool hashTable::setPointer(std::string_view key, void *pv) {
+    bool exists;
+    Hash h;
+
+    std::tie(exists, h) = this->findPos(key);
+    if (!exists)
+        return false;
+
+    this->data[h].pv = pv;
+    return true;
+}
+
+std::pair<bool, void*> hashTable::getPointer(std::string_view key) {
+    bool exists;
+    Hash h;
+
+    std::tie(exists, h) = this->findPos(key);
+    if (!exists)
+        return {false, nullptr};
+
+    return {true, this->data[h].pv};
+}
+
+bool hashTable::remove(std::string_view key) {
+    bool exists; Hash h;
+    std::tie(exists, h) = findPos(key);
+    if (!exists) return false;
+
+    data[h].isDeleted = true;
+    filled--;
+    return true;
+}
---
+++ a2/heap.cpp
@@ -0,0 +1,127 @@
+#include "heap.h"
+
+#include <iostream>
+
+heap::heap(int capacity) : mapping(capacity*2) {
+    data.reserve(capacity + 1);
+    currentSize = 0;
+}
+
+int heap::insert(std::string_view id, int key, void *pv) {
+    if (currentSize+1 > data.capacity()) return 1;
+    if (mapping.contains(id)) return 2;
+    
+    Node n;
+    n.id = id;
+    n.key = key;
+    n.pData = pv;
+
+    data[++currentSize] = n;
+    if (mapping.insert(id, &data[currentSize])) {
+        // should not be possible to get return code 1, because we checked
+        // contains() earlier.
+        std::cerr << "Error allocating memory for rehash! Exiting.\n";
+        abort();
+    }
+
+    percolateUp(currentSize);
+
+    return 0;
+}
+
+int heap::setKey(std::string_view id, int key) {
+    bool exists; void* v;
+
+    std::tie(exists, v) = mapping.getPointer(id);
+    if (!exists) return 1;
+
+    Node *n = static_cast<Node *>(v);
+    int old_key = n->key;
+    n->key = key;
+
+    if (key > old_key)
+        percolateDown(getPos(n));
+    else if (key < old_key)
+        percolateUp(getPos(n));
+
+    return 0;
+}
+
+void heap::percolateUp(int hole) {
+    data[0] = data[hole];
+
+    for (; data[0].key < data[hole / 2].key; hole /= 2) {
+        mapping.setPointer(data[hole / 2].id, &data[hole]);
+        data[hole] = data[hole / 2];
+    }
+
+    data[hole] = data[0];
+    mapping.setPointer(data[hole].id, &data[hole]);
+}
+
+void heap::percolateDown(int hole) {
+    int child;
+
+    Node tmp = data[hole];
+
+    for (; hole * 2 < currentSize; hole = child) {
+        child = hole * 2;
+        if (child != currentSize && data[child+1].key < data[child].key) {
+            child++;
+        }
+        if (data[child].key < tmp.key) {
+            data[hole] = data[child];
+            mapping.setPointer(data[hole].id, &data[hole]);
+        }
+        else {
+            break;
+        }
+    }
+
+    data[hole] = tmp;
+    mapping.setPointer(data[hole].id, &data[hole]);
+}
+
+int heap::deleteMin(std::string *pId, int *pKey, void *ppData) {
+    Node* n = &data[1];
+
+    if (pId) *pId = n->id;
+    if (pKey) *pKey = n->key;
+    if (ppData) *(static_cast<void **>(ppData)) = n->pData;
+
+    if (currentSize == 0) return 1;
+
+    mapping.remove(data[1].id);
+    data[1] = data[currentSize--];
+    percolateDown(1);
+
+    return 0;
+}
+
+int heap::remove(std::string_view id, int *pKey, void *ppData) {
+    bool exists; void* v;
+    std::tie(exists, v) = mapping.getPointer(id);
+
+    if (!exists) return 1;
+
+    Node *n = static_cast<Node*>(v);
+
+    if (pKey) *pKey = n->key;
+    if (ppData) *(static_cast<void **>(ppData)) = n->pData;
+    
+    setKey(id, data[1].key-1);
+
+    deleteMin();
+    return 0;
+}
+
+int heap::getPos(const heap::Node *pn) {
+    return pn - &data[0];
+}
+
+void heap::dumptable() {
+    for (int i = 1; i < currentSize + 1; i++) {
+        Node *n = &data[i];
+        std::cout << "ID: \"" << n->id << "\" Key: " << n->key << std::endl;
+    }
+}
